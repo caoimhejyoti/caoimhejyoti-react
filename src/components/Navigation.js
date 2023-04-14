b3d159c8350a0427204aa7e9fda5a10d3f686d43
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-14 16:07:33 +0800

    consolidating styles

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -10,16 +10,14 @@ const logoStyle = {
 // We assign them to their own variable names
 function Navigation({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav row  align-items-center">
+    <ul className="nav row ">
       <li className="navigation nav-item col ">
         <a
           href="#about"
           onClick={() => handlePageChange("About")}
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={
-            currentPage === "About"
-              ? "nav-link active d-flex justify-content-center"
-              : "nav-link d-flex justify-content-center"
+            currentPage === "About" ? "nav-link active " : "nav-link d-flex "
           }
         >
           About
@@ -31,25 +29,19 @@ function Navigation({ currentPage, handlePageChange }) {
           onClick={() => handlePageChange("Portfolio")}
           // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={
-            currentPage === "Portfolio"
-              ? "nav-link active d-flex justify-content-center"
-              : "nav-link d-flex justify-content-center"
+            currentPage === "Portfolio" ? "nav-link active " : "nav-link "
           }
         >
           Portfolio
         </a>
       </li>
-      <li className=" nav-item col-6 ">
+      <li className="navigation nav-item col ">
         <a
           href="#home"
           onClick={() => handlePageChange("Home")}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={
-            currentPage === "Home"
-              ? "nav-link active d-flex justify-content-center"
-              : "nav-link d-flex justify-content-center"
-          }
+          className={currentPage === "Home" ? "nav-link active" : "nav-link "}
         >
           <img style={logoStyle} src={logo} alt="CaoimheJyoti logo" />
         </a>
@@ -60,9 +52,7 @@ function Navigation({ currentPage, handlePageChange }) {
           onClick={() => handlePageChange("Resume")}
           // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={
-            currentPage === "Resume"
-              ? "nav-link active d-flex justify-content-center"
-              : "nav-link d-flex justify-content-center"
+            currentPage === "Resume" ? "nav-link active " : "nav-link "
           }
         >
           Resume
@@ -74,9 +64,7 @@ function Navigation({ currentPage, handlePageChange }) {
           onClick={() => handlePageChange("Contact")}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={
-            currentPage === "Contact"
-              ? "nav-link active d-flex justify-content-center"
-              : "nav-link d-flex justify-content-center"
+            currentPage === "Contact" ? "nav-link active " : "nav-link "
           }
         >
           Contact
