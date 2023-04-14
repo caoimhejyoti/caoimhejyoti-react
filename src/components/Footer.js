a946cf6f6e8b0e0753fe98cef9687a20490e5d85
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-14 16:27:34 +0800

    updating footer style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="footer">This will be my footer</div>;
+  return <div className="footer fixed-bottom">This will be my footer</div>;
 }
