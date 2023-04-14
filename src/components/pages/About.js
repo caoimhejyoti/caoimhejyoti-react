44e11b4286065ee1f27076fada22e6166e183224
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-14 16:07:44 +0800

    adding hero base

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,5 +1,20 @@
 import React from "react";
+import laugh from "../../assets/img/laugh - Edited.png";
 
+const laughStyle = {
+  height: "30rem",
+  borderRadius: "50%",
+};
 export default function About() {
-  return <h1 className="myh1">About</h1>;
+  return (
+    <div>
+      <div className="hero">
+        <img style={laughStyle} src={laugh} alt="CaoimheJyoti laughing" />
+        <h1 className="myh1">
+          Hey! <br />I am Caoimhe Jyoti
+        </h1>
+      </div>
+      <div></div>
+    </div>
+  );
 }
