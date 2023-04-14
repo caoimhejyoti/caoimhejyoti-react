eeca9fedda4ce1cea0c8a45556675b3c53edbcea
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-14 16:20:45 +0800

    working on a bubble behind image

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,14 +2,21 @@ import React from "react";
 import laugh from "../../assets/img/laugh - Edited.png";
 
 const laughStyle = {
+  // backgroundColour: "white",
   height: "30rem",
   borderRadius: "50%",
+};
+
+const bubbleStyle = {
+  backgroundColour: "#0d324d",
 };
 export default function About() {
   return (
     <div>
       <div className="hero">
-        <img style={laughStyle} src={laugh} alt="CaoimheJyoti laughing" />
+        <div style={bubbleStyle}>
+          <img style={laughStyle} src={laugh} alt="CaoimheJyoti laughing" />
+        </div>
         <h1 className="myh1">
           Hey! <br />I am Caoimhe Jyoti
         </h1>
