e2ad271a3a3c7fd81f300a603867fa7acd280ddb
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-17 14:11:00 +0800

    adding img to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,26 @@
 import React from "react";
+import github from "./../assets/img/logos/GitHub-Mark-Light-120px-plus.png";
+import linkedin from "./../assets/img/logos/In-White-128-┬«@2x.png";
 
 export default function Footer() {
-  return <div className="footer fixed-bottom">This will be my footer</div>;
+  return (
+    <div className="footer fixed-bottom">
+      <a
+        href="https://github.com/caoimhejyoti"
+        target="blank"
+        className="footer-img"
+      >
+        <img src={github} alt="Github logo" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/caoimhe-young-84b58b61/"
+        target="blank"
+        className="footer-img"
+      >
+        <img src={linkedin} alt="Linkedin logo" />
+      </a>
+      <br />
+      <h4>© CaoimheJyoti</h4>
+    </div>
+  );
 }
