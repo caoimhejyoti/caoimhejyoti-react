3a1571df44382c6174396121f3e97feb3a8ac841
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-17 14:20:12 +0800

    adding instagram logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import github from "./../assets/img/logos/GitHub-Mark-Light-120px-plus.png";
 import linkedin from "./../assets/img/logos/In-White-128-┬«@2x.png";
+import instagram from "./../assets/img/logos/Instagram_Glyph_White.png";
 
 export default function Footer() {
   return (
@@ -18,6 +19,13 @@ export default function Footer() {
         className="footer-img"
       >
         <img src={linkedin} alt="Linkedin logo" />
+      </a>
+      <a
+        href="https://www.instagram.com/iamlcam/"
+        target="blank"
+        className="footer-img"
+      >
+        <img src={instagram} alt="Instagram logo" />
       </a>
       <br />
       <h4>© CaoimheJyoti</h4>
