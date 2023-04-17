6487dbfd02c96651f9d56ec1584ff203c5f86226
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-17 17:56:38 +0800

    adding resume css and detail

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./../../style/Resume.css";
 
 // const fileAddress = `https://localhost:3000/resume_test5.pdf`;
 
@@ -25,7 +26,41 @@ export default function Resume() {
       </div>
       <div>
         <p>Interested in a full resume? Download it here.</p>
-        <button href = "https://github.com/caoimhejyoti/caoimhejyoti-react/raw/master/src/assets/downloads/resume-test5.pdf" className="card-button" target="blank">Download</button>
+        <a
+          href="https://github.com/caoimhejyoti/caoimhejyoti-react/raw/main/src/assets/downloads/resume_test5.pdf"
+          className="card-button link"
+          target="blank"
+        >
+          Download
+        </a>
+      </div>
+      <div className="resume-container">
+        <div className="resume-card">
+          <h2>Front-end Experience</h2>
+          <p></p>
+        </div>
+        <div className="resume-card">
+          <h2>Back-end Experience</h2>
+          <p></p>
+        </div>
+        {/* <div className="resume-card">
+          <h2>Languages</h2>
+          <ul>
+          <li>HTML 5<li>
+          <li>CSS 3<li>
+          <li>JavaScript<li>
+          <li>MySQL<li>
+          <li>Sequalize<li>
+          <li>MongoDB<li>
+          <li>NodeJS<li>
+          <li>Express<li>
+          <li>npm<li>
+          <li>React<li>
+          <li>Git<li>
+          <li>GitHub<li>
+          <li>GitHub<li>
+          
+        </div> */}
       </div>
     </div>
   );
