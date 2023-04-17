a829615d45671512e03d498b6d511e43c90601d2
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-17 17:35:45 +0800

    creating downloadable file

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,32 @@
 import React from "react";
 
+// const fileAddress = `https://localhost:3000/resume_test5.pdf`;
+
 export default function Resume() {
-  return <h1 className="myh1">Resume</h1>;
+  // const download = (url) => {
+  //   fetch(url)
+  //     .then((res) => res.blob())
+  //     .then((blob) => {
+  //       const blobURL = window.URL.createObjectURL(new Blob([blob]));
+  //       const fileName = url.split("/").pop();
+  //       const aTag = document.createElement("a");
+  //       aTag.href = blobURL;
+  //       aTag.setAttribute("download", fileName);
+  //       document.body.appendChild(aTag);
+  //       aTag.click();
+  //       aTag.remove();
+  //     });
+  // };
+
+  return (
+    <div>
+      <div>
+        <h1 className="myh1">Resume</h1>
+      </div>
+      <div>
+        <p>Interested in a full resume? Download it here.</p>
+        <button href = "https://github.com/caoimhejyoti/caoimhejyoti-react/raw/master/src/assets/downloads/resume-test5.pdf" className="card-button" target="blank">Download</button>
+      </div>
+    </div>
+  );
 }
