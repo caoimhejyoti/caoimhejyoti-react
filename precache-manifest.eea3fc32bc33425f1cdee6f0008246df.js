85b9f2dbfd526860fc70cb10c3ecfa4c9e100dd3
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-18 22:09:32 +0800

    Updates

---
+++ precache-manifest.eea3fc32bc33425f1cdee6f0008246df.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ec4e1b86ce726facc31",
+    "url": "/caoimhejyoti-react/static/css/main.a7d22161.chunk.css"
+  },
+  {
+    "revision": "0ec4e1b86ce726facc31",
+    "url": "/caoimhejyoti-react/static/js/main.0ec4e1b8.chunk.js"
+  },
+  {
+    "revision": "ad23510e29ca5b353c35",
+    "url": "/caoimhejyoti-react/static/css/1.c3687ae1.chunk.css"
+  },
+  {
+    "revision": "ad23510e29ca5b353c35",
+    "url": "/caoimhejyoti-react/static/js/1.ad23510e.chunk.js"
+  },
+  {
+    "revision": "01ba510dc011f1719fd8",
+    "url": "/caoimhejyoti-react/static/js/runtime~main.01ba510d.js"
+  },
+  {
+    "revision": "92b57d5df83a23ac43430be9509fc44a",
+    "url": "/caoimhejyoti-react/static/media/caoimheJyoti-logo.92b57d5d.png"
+  },
+  {
+    "revision": "e53e1364992d36be750aa6e9dc429572",
+    "url": "/caoimhejyoti-react/static/media/laugh - Edited.e53e1364.png"
+  },
+  {
+    "revision": "8dd4588aa732d6b0e3756ffd41c0909e",
+    "url": "/caoimhejyoti-react/static/media/buddy-up-screenshot.8dd4588a.png"
+  },
+  {
+    "revision": "7c3747f845c78f3a9bf34a187b1fa700",
+    "url": "/caoimhejyoti-react/static/media/ppp-screenshot.7c3747f8.png"
+  },
+  {
+    "revision": "c004fdb969655b56c505bf9461fbc193",
+    "url": "/caoimhejyoti-react/static/media/social-media-api-screenshot.c004fdb9.png"
+  },
+  {
+    "revision": "bcde74f139dddb3e34fc11fc985ca090",
+    "url": "/caoimhejyoti-react/static/media/coding-with-caoimhe-screenshot.bcde74f1.png"
+  },
+  {
+    "revision": "df6d0fd43adfef7ceaa2340b8957eaa4",
+    "url": "/caoimhejyoti-react/static/media/team-profile-creator-screenshot.df6d0fd4.png"
+  },
+  {
+    "revision": "21812652557ff6b5f48102bbcc71fefa",
+    "url": "/caoimhejyoti-react/static/media/e-commerce-platform-screenshot.21812652.png"
+  },
+  {
+    "revision": "8110c5fdf0db5b5d9d855d1de65f2f40",
+    "url": "/caoimhejyoti-react/index.html"
+  }
+];
