5df8d968dbb617cbdd7eabbf76ea42dd23c9e984
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-18 14:36:04 +0800

    creating contactform components

---
+++ src/components/Contact/nameInput.js
@@ -0,0 +1,8 @@
+{/* <input
+                value={firstName}
+                name="firstName"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="First Name"
+                className="form-control"
+              /> */}
