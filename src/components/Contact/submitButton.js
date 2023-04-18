4911c81a4c06329f2906b0160d81610de3e39213
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-18 17:22:30 +0800

    adding on click alert

--- src/components/Contact/submitButton.js
+++ src/components/Contact/submitButton.js
@@ -2,12 +2,19 @@ import React from "react";
 import Button from "@mui/material/Button";
 import { ThemeProvider } from "@mui/material/styles";
 import inputTheme from "./../../style/theme";
-
+import { Alert } from "@mui/material";
 
 export default function BasicButtons() {
   return (
     <ThemeProvider theme={inputTheme}>
-      <Button variant="contained" id="submit-button" className="primary">
+      <Button
+        variant="contained"
+        id="submit-button"
+        className="primary"
+        onClick={ () =>{
+          alert("Thanks for getting in touch!");
+        }}
+      >
         Submit
       </Button>
     </ThemeProvider>
