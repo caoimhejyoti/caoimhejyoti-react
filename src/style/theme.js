eb19d98394435674d86c47af302765b2254de5fa
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-18 16:59:57 +0800

    creating theme

---
+++ src/style/theme.js
@@ -0,0 +1,11 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const inputTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#01463c",
+    },
+  },
+});
+
+export default inputTheme;
