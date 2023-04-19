3af6fd5cfe6d5efa9163a8bd45e449f99360652d
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-19 22:45:42 +0800

    grammer updated

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,17 +1,17 @@
 import React from "react";
 import PortfolioCards from "./PortfolioCards";
 
-
-
 export default function Portfolio() {
   return (
     <div>
       <div>
         <h1 className="myh1">Portfolio</h1>
-        <p>Want to see an example of my work? <br/> Here is your chance. </p>
+        <p>
+          Want to see examples of my work? <br /> Here is your chance.{" "}
+        </p>
       </div>
       <div>
-        <PortfolioCards className = "container"  />
+        <PortfolioCards className="container" />
       </div>
     </div>
   );
