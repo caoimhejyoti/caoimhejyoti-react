5a0a592a4f4065d6e2c1c0f6bd3e8cb197e4c08d
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-19 22:45:11 +0800

    updating alert + disabled?

--- src/components/Contact/submitButton.js
+++ src/components/Contact/submitButton.js
@@ -1,22 +1,57 @@
-import React from "react";
-import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import inputTheme from "./../../style/theme";
-import { Alert } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  // DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  useMediaQuery,
+} from "@mui/material";
+import { useTheme } from "@mui/material/styles";
 
 export default function BasicButtons() {
+  const [open, setOpen] = useState(false);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <ThemeProvider theme={inputTheme}>
       <Button
         variant="contained"
         id="submit-button"
         className="primary"
-        onClick={ () =>{
-          alert("Thanks for getting in touch!");
-        }}
+        onClick={handleClickOpen}
       >
         Submit
       </Button>
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialoge-title">
+          Thanks for getting in touch!
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            There is only one me so please give me a week to reply before
+            sending another message.
+          </DialogContentText>
+        </DialogContent>
+        {/* <DialogActions></DialogActions> */}
+      </Dialog>
     </ThemeProvider>
   );
 }
