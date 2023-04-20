cb2009ac3559ffe07715c183ea470fe48be567d8
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-20 17:22:58 +0800

    updating about page css + hero content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,27 +1,34 @@
 import React from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import inputTheme from "./../style/theme";
+
+// IMGs
 import laugh from "../assets/img/laugh - Edited.png";
+import eyes from "../assets/img/eyes-large.png";
+import blob from "../assets/img/blobs/blob_1_green.png";
 
 const laughStyle = {
-  // backgroundColour: "white",
   height: "30rem",
   borderRadius: "50%",
 };
 
 const bubbleStyle = {
-  backgroundColour: "#0d324d",
+  bacgroundImage: blob,
+  // backgroundColour: "#0d324d",
 };
 export default function About() {
   return (
     <div>
-      <div className="hero">
-        <h1 className="myh1">
-          Hey! <br />I am Caoimhe Jyoti
-        </h1>
-        <div style={bubbleStyle}>
-          <img style={laughStyle} src={laugh} alt="CaoimheJyoti laughing" />
+      <ThemeProvider theme={inputTheme}>
+        <div className="hero eyes">
+          <h1 id="myh1">
+            Hey! <br />I am Caoimhe Jyoti
+          </h1>
+          <div>
+            {/* <img style={laughStyle} src={eyes} alt="CaoimheJyoti laughing" /> */}
+          </div>
         </div>
-      </div>
-      {/* <div></div> */}
+      </ThemeProvider>
     </div>
   );
 }
