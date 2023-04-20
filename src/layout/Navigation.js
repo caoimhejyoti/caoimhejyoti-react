b2ce9f7555a76240567e9c46e33dc74aeb800b31
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-20 15:13:04 +0800

    clear dead code

--- src/layout/Navigation.js
+++ src/layout/Navigation.js
@@ -9,8 +9,6 @@ const logoStyle = {
   height: "10rem",
 };
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 function Navigation({ currentPage, handlePageChange }) {
   function navigationActiveWhen(pageName) {
     return currentPage === pageName ? " navigation-active " : "navigation ";
@@ -57,6 +55,7 @@ function Navigation({ currentPage, handlePageChange }) {
               >
                 Resume
               </Nav.Item>
+
               <Nav.Item
                 href="#contact"
                 onClick={() => handlePageChange("Contact")}
@@ -73,66 +72,3 @@ function Navigation({ currentPage, handlePageChange }) {
 }
 
 export default Navigation;
-
-// NOTE: Original NAVBAR
-// <ul className="nav row ">
-//   <li className="navigation nav-item col ">
-//     <a
-//       href="#about"
-//       onClick={() => handlePageChange("About")}
-//       // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-//       className={
-//         currentPage === "About" ? " navigation-active " : "navigation "
-//       }
-//     >
-//       About
-//     </a>
-//   </li>
-//   <li className="navigation nav-item col ">
-//     <a
-//       href="#portfolio"
-//       onClick={() => handlePageChange("Portfolio")}
-//       // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-//       className={
-//         currentPage === "Portfolio" ? "navigation-active " : "navigation "
-//       }
-//     >
-//       Portfolio
-//     </a>
-//   </li>
-//   <li className="navigation nav-item col ">
-//     <a
-//       href="#about"
-//       onClick={() => handlePageChange("About")}
-//       // This is a conditional (ternary) operator that checks to see if the current page is "About"
-//       // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-//       className={currentPage === "About" ? "nav-link active" : "nav-link "}
-//     >
-//       <img style={logoStyle} src={logo} alt="CaoimheJyoti logo" />
-//     </a>
-//   </li>
-//   <li className="navigation nav-item col">
-//     <a
-//       href="#resume"
-//       onClick={() => handlePageChange("Resume")}
-//       // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-//       className={
-//         currentPage === "Resume" ? "navigation-active " : "navigation "
-//       }
-//     >
-//       Resume
-//     </a>
-//   </li>
-//   <li className="navigation nav-item col">
-//     <a
-//       href="#contact"
-//       onClick={() => handlePageChange("Contact")}
-//       // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-//       className={
-//         currentPage === "Contact" ? "navigation-active " : "navigation  "
-//       }
-//     >
-//       Contact
-//     </a>
-//   </li>
-// </ul>
