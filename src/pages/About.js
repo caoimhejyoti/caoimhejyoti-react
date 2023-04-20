5e80e7af3e484d9aa7550f1015c1b1f2f4f744da
caoimhejyoti <caoimhejyoti@gmail.com>
2023-04-20 22:33:23 +0800

    updating about page text and eyes position

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,7 @@ import inputTheme from "./../style/theme";
 import laugh from "../assets/img/laugh - Edited.png";
 import eyes from "../assets/img/eyes-large.png";
 import blob from "../assets/img/blobs/blob_1_green.png";
+import { Typography } from "@mui/material";
 
 const laughStyle = {
   height: "30rem",
@@ -24,9 +25,20 @@ export default function About() {
           <h1 id="myh1">
             Hey! <br />I am Caoimhe Jyoti
           </h1>
-          <div>
-            {/* <img style={laughStyle} src={eyes} alt="CaoimheJyoti laughing" /> */}
-          </div>
+        </div>
+        <div>
+          <Typography>
+            {" "}
+            I am a colourful amalgamation of working in the events industry and
+            a love for puzzels. <br />
+            <br />
+            I am new to the work of Fullstack development but that is not
+            stopping me! I am a keen learner and cannot wait to get stuck into
+            my next projec.
+            <br />
+            <br />
+            Lets make your ideas into a reality!
+          </Typography>
         </div>
       </ThemeProvider>
     </div>
